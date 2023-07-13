6413e2a9c3f401721b0a32a4721818b203059029
symetahybrid <erik.willemse@symeta-hybrid.com>
2023-07-13 23:03:52 +0200

    Update helloworld.js

--- helloworld.js
+++ helloworld.js
@@ -1,4 +1,2 @@
-alert('Hello, World!');
+alert('Hello, World! This is a test to load external javascript.');
 console.log("Just said hello!")
-console.log("can I change code in the web UI?")
-console.log("seems like you can... and then fetch it back and make changes in Nova again?")
