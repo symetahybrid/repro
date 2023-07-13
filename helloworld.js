c784027ea38e56baba643f7b8758947f497e63b5
symetahybrid <erik.willemse@symeta-hybrid.com>
2023-07-13 20:56:06 +0200

    Update helloworld.js
    updated and added another line

--- helloworld.js
+++ helloworld.js
@@ -1,3 +1,4 @@
 alert('Hello, World!');
 console.log("Just said hello!")
 console.log("can I change code in the web UI?")
+console.log("seems like you can... and then fetch it back and make changes in Nova again?")
